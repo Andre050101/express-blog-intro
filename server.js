d6809f1cbad08a9bd093bdd415a849f5b572cfa5
Andre050101 <agiancristiano90@gmail.com>
2024-12-02 18:37:50 +0100

    2 find added

--- server.js
+++ server.js
@@ -14,14 +14,32 @@ app.get('/', (req, res) => {
 
 //Rotta bacheca
 app.get('/bacheca', (req, res, next) => {
-    if (!posts || posts.length === 0) {
+    let { titolo, tag } = req.query; // Estrai i parametri dalla query string
+
+    let risultati = posts; // Usa l'array dei post esistenti
+
+    // Filtro per titolo (se fornito)
+    if (titolo) {
+        risultati = risultati.filter(post =>
+            post.titolo.toLowerCase().includes(titolo.toLowerCase())
+        );
+    }
+
+    // Filtro per tag (se fornito)
+    if (tag) {
+        risultati = risultati.filter(post =>
+            post.tags && post.tags.includes(tag.toLowerCase())
+        );
+    }
+
+    if (risultati.length === 0) {
         return res.status(404).json({
-            message: 'Nessun post disponibile'
+            message: 'Nessun post trovato con i parametri specificati'
         });
     }
     res.json({
-        posts: posts,
-        conteggio: posts.length
+        posts: risultati,
+        conteggio: risultati.length
     });
 });
 
@@ -36,4 +54,4 @@ app.use((req, res, next) => {
 //Avvia server
 app.listen(port, () => {
     console.log(`Server avviato su http://localhost:${port}`);
-})
+});
