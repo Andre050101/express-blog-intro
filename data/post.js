1ec8e62219ee81c3d730ecc616e9217b6fa472fe
Andre050101 <agiancristiano90@gmail.com>
2024-12-02 16:01:20 +0100

    route /bacheca created

--- data/post.js
+++ data/post.js
@@ -3,31 +3,31 @@ const posts = [
     {
         titolo: "Primo post",
         contenuto: "Questo è il contenuto del primo post",
-        immagine: "./assets/ciambellone.jpeg",
+        immagine: "/assets/ciambellone.jpeg",
         tags: ["dolci", "zucchero"]
     },
     {
         titolo: "Secondo post",
         contenuto: "Questo è il contenuto del secondo post",
-        immagine: "./assets/cracker_barbabietola.jpeg",
+        immagine: "/assets/cracker_barbabietola.jpeg",
         tags: ["dolci", "zucchero"]
     },
     {
         titolo: "Terzo post",
         contenuto: "Questo è il contenuto del terzo post",
-        immagine: "./assets/pane_fritto_dolce.jpeg",
+        immagine: "/assets/pane_fritto_dolce.jpeg",
         tags: ["dolci", "zucchero"]
     },
     {
         titolo: "Quarto post",
         contenuto: "Questo è il contenuto del quarto post",
-        immagine: "./assets/pasta_barbabietola.jpeg",
+        immagine: "/assets/pasta_barbabietola.jpeg",
         tags: ["dolci", "zucchero"]
     },
     {
         titolo: "Quinto post",
         contenuto: "Questo è il contenuto del quinto post",
-        immagine: "./assets/torta_paesana.jpeg",
+        immagine: "/assets/torta_paesana.jpeg",
         tags: ["dolci", "zucchero"]
     }
 ];
--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express'); //Richiamo express
 const path = require('path');//Richiamo path per versatilità percorsi su diversi sistemi operativi
+const posts = require('./data/post');
 const app = express(); //Variabile che contene express
 const port = 3000; //porta
+
+//Serviamo asset statici di cartella public
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Rotta principale
 app.get('/', (req, res) => {
     res.send('Server del mio blog');
 });
+
+//Rotta bacheca
+app.get('/bacheca', (req, res) => {
+    res.json({
+        posts: posts,
+        conteggio: posts.length
+    });
+});
+
 
 //Avvia server
 app.listen(port, () => {
